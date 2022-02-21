efbf04d6fa0cfd99cb45793489f737da6f447777
Zhou Zheng <38266008+ZhouZhengCarlos@users.noreply.github.com>
2022-02-20 22:58:17 -0600

    Menu bar and animations completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import Portfolio from './components/portfolio/Portfolio'
 import Testimonials from './components/testimonials/Testimonials'
 import Works from './components/works/Works'
 import Contact from './components/contact/Contact'
+import Menu from './components/menu/Menu'
 import "./app.scss"
-
+import { useState } from "react" 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className="app">
-    <Topbar/>
+    <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+    <Menu  menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     <div className='sections'>
       <Intro/>
       <Portfolio/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,8 +1,8 @@
 import './topbar.scss'
 import {Person, Mail} from '@material-ui/icons'
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className='topbar'>
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className='wrapper'>
           <div className='left'>
               <a href="#intro" className='logo'>Colorblind Pixel.</a>
@@ -15,7 +15,13 @@ export default function Topbar() {
               <span>carloszhou0@gmail.com</span>
             </div>
           </div>
-          <div className="right"></div>
+          <div className="right">
+              <div className='hamburger' onClick={()=>setMenuOpen(!menuOpen)}>
+                <span className='line1'></span>
+                <span className='line2'></span>
+                <span className='line3'></span>
+              </div>
+          </div>
       </div>
     </div>
   )
