942265eff47bc48c4df2fdcc7cd4105c1cba51f1
Carlos Zhou <carloszhou0@gmail.com>
2023-06-08 13:34:24 -0600

    test snapping

---
+++ src/components/shop/ItemCard/ItemCard.jsx
@@ -0,0 +1,25 @@
+// import "./itemCard.scss"
+
+// export default function PortfolioList() {
+//   return (
+//     <div className="item">
+//               <img
+//                 src={d.URL}
+//                 alt=""
+//                 onClick={() => {
+//                   setOpen(true);
+//                   setImage(d.URL);
+//                   setDownload(selected !== "free");
+//                 }}
+//               />
+//               <h3
+//                 onClick={() => {
+//                   setOpen(true);
+//                   setImage(d.URL);
+//                 }}
+//               >
+//                 {d.Name}
+//               </h3>
+//             </div>
+//   )
+// }
