eb0901398cca6a68dc32eefba478a22e9640b61b
zhouzhengcarlos <carloszhou0@gmail.com>
2023-06-07 18:15:44 -0600

    Adding contact us functionality

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
 import "./contact.scss";
+import { serverTimestamp, addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebase";
 import SocialMediaIcon from "../socialMediaLinks/SocialMediaIcon";
 
 export default function Contact() {
   const [message, setMessage] = useState(false);
 
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState("");
+  const [text, setText] = useState("");
+
+  async function submitMessage() {
+    const collectionRef = collection(db, "Messages");
+      try {
+        await addDoc(collectionRef, {
+          Message: text,
+          Email: email,
+          timestamp: serverTimestamp()
+        })
+      } catch (err) {
+        console.log(err);
+      }
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    await submitMessage();
     setMessage(true);
   };
   return (
@@ -14,10 +33,10 @@ export default function Contact() {
       <div className="right">
         <h2>Contact me!</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Your Email" />
-          <textarea placeholder="Message"></textarea>
+          <input type="email" placeholder="Your Email" required onChange={(e) => setEmail(e.target.value)}/>
+          <textarea placeholder="Message" required onChange={(e) => setText(e.target.value)}></textarea>
           <button type="submit">Send</button>
-          {message && <span>Thanks, I'll reply ASAP :)</span>}
+          {message && <span>Thanks, I'll reply ASAP</span>}
           <h3><center>Contact me on Social Media!</center></h3>
           <div className="socialMedia">
             <SocialMediaIcon
