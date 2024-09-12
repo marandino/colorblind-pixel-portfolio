55139f56acf4802ed5cad3b32b348d64639e22c4
Deyner Cruz <nenedac98@gmail.com>
2024-09-12 16:20:46 -0600

    adds: typescript support to ItemCard.tsx

--- src/components/shop/ItemCard/ItemCard.tsx
+++ src/components/shop/ItemCard/ItemCard.tsx
@@ -1,15 +1,30 @@
 import "./itemCard.scss";
 import { Link } from "react-router-dom";
-export default function ItemCard({ itemId, title, price, imageURL, isAvailable }) {
+
+interface ItemCardProps {
+  itemId: string;
+  title: string;
+  price: number;
+  imageURL: string;
+  isAvailable: boolean;
+}
+
+export default function ItemCard({
+  itemId,
+  title,
+  price,
+  imageURL,
+  isAvailable,
+}: ItemCardProps): JSX.Element {
   return (
     <Link to={`/shop/${itemId}`} style={{ textDecoration: "none", color: "black" }}>
       <div className="itemCard">
         <div className="imageContainer">
-          <img src={imageURL} alt="" />
+          <img src={imageURL} alt={title} />
         </div>
         <div className="description">
           <h3>{title}</h3>
-          { !isAvailable && <span> Not Available</span>}
+          {!isAvailable && <span> Not Available</span>}
           <span>₡ {new Intl.NumberFormat().format(price)} CRC</span>
         </div>
       </div>
