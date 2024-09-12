20daaf619b7fa408bc8ec5b32d4b214ab4747622
Deyner Cruz <nenedac98@gmail.com>
2024-09-12 16:36:17 -0600

    adds: typescript support to PortfolioTabs.tsx

---
+++ src/components/portfolioTabs/PortfolioTabs.tsx
@@ -0,0 +1,24 @@
+import "./portfolioTabs.scss";
+
+interface PortfolioListProps {
+  id: string;
+  title: string;
+  active: boolean;
+  setSelected: (id: string) => void;
+}
+
+export default function PortfolioList({
+  id,
+  title,
+  active,
+  setSelected,
+}: PortfolioListProps): JSX.Element {
+  return (
+    <li
+      className={active ? "portfolioList active" : "portfolioList"}
+      onClick={() => setSelected(id)}
+    >
+      {title}
+    </li>
+  );
+}
