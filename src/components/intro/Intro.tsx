0dc041caa47e88058184167ccfee6c86fcfe7c39
Deyner Cruz <158535310+nenedac@users.noreply.github.com>
2024-09-12 13:55:15 -0600

    CBP-16 - Typescript Intro (#4)
    * adds: types for ityped
    
    * adds: typescript support to Intro.tsx
    
    * fix: package lock dev dependency
    
    * fix: replace local image for cloudinary link
    
    * fix: escaped '
    
    ---------
    
    Co-authored-by: Chiyen Venegas <chi@marandino.dev>

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -3,29 +3,31 @@ import "./intro.scss";
 import { init } from "ityped";
 import { useEffect, useRef } from "react";
 import SocialMediaSection from "../socialMediaLinks/SocialMediaSection";
+
 export default function Intro() {
-  const textRef = useRef();
+  const textRef = useRef<HTMLSpanElement>(null);
 
   useEffect(() => {
-    init(textRef.current, {
-      showCursor: true,
-      backDelay: 1500,
-      backSpeed: 60,
-      strings: ["Pixel Art", "Pixel Art Logos", "Pixel Art Animation"],
-    });
+    if (textRef.current) {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500,
+        backSpeed: 60,
+        strings: ["Pixel Art", "Pixel Art Logos", "Pixel Art Animation"],
+      });
+    }
   }, []);
 
   return (
     <div className="intro" id="intro">
       <div className="left">
         <div className="imgContainer">
-          <img src="assets/cevicheriaMagikarp.png" alt="" />
+          <img src="https://res.cloudinary.com/dkhpxyxnt/image/upload/v1725932096/94e95e1d-2e1a-4375-81ce-042132ff5f0a.png" alt="" />
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
-          <h2>Hi there, I'm</h2>
-          <h1>Colorblind Pixel</h1>
+        <h2>Hi there, I&apos;m</h2>          <h1>Colorblind Pixel</h1>
           <h3>
             <span ref={textRef}></span>
           </h3>
