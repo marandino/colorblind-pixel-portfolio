cc84e99107bddf6293febbf7cee14bd6a458a978
Deyner Cruz <nenedac98@gmail.com>
2024-09-16 11:22:07 -0600

    fix: testing possible fix

--- src/components/socialMediaLinks/SocialMediaSection.tsx
+++ src/components/socialMediaLinks/SocialMediaSection.tsx
@@ -4,18 +4,18 @@ export default function SocialMediaSection() {
   return (
     <div className="socialMediaLogos">
       <SocialMediaIcon
-        socialMedia={"fa-facebook"}
-        URL={"https://www.facebook.com/ZhouIsDrawing"}
+        socialMedia="fa-facebook"
+        URL="https://www.facebook.com/ZhouIsDrawing"
         ariaLabel="Visit my Facebook page"
       />
       <SocialMediaIcon
-        socialMedia={"fa-instagram"}
-        URL={"https://www.instagram.com/colorblind_pixel/"}
+        socialMedia="fa-instagram"
+        URL="https://www.instagram.com/colorblind_pixel/"
         ariaLabel="Visit my Instagram profile"
       />
       <SocialMediaIcon
-        socialMedia={"fa-twitter"}
-        URL={"https://twitter.com/ColorblindPixl"}
+        socialMedia="fa-twitter"
+        URL="https://twitter.com/ColorblindPixl"
         ariaLabel="Visit my Twitter profile"
       />
     </div>
