6f82bab8c45dd6662e49fd6d80de7d2c5355fd5d
K3iiX <karaya@marandino.dev>
2024-09-17 19:04:34 -0600

    adds: useCloudinary hook to setup shop images and improve styles

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,55 +1,51 @@
 import React, { useState } from 'react';
+import CloudinaryImage from './CloudinaryImage';
+import { useCloudinaryImages } from '../hooks/useCloudinaryImages';
 
-interface ShopItem {
-    id: number;
-    image: string;
-    name: string;
-}
-
-const items: ShopItem[] = [
-    { id: 1, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246933/merch/emalomj0s3u7d8xhokrv', name: 'Item 1'},
-    { id: 2, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246933/merch/emalomj0s3u7d8xhokrv', name: 'Item 2'},
-    { id: 3, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246933/merch/emalomj0s3u7d8xhokrv', name: 'Item 3'},
-    { id: 4, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246933/merch/emalomj0s3u7d8xhokrv', name: 'Item 4'},
-    { id: 5, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246929/merch/gfctd1ynsdcpchzvgukc', name: 'Item 5'},
-    { id: 6, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246929/merch/gfctd1ynsdcpchzvgukc', name: 'Item 6'},
-    { id: 7, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246929/merch/gfctd1ynsdcpchzvgukc', name: 'Item 7'},
-    { id: 8, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246929/merch/gfctd1ynsdcpchzvgukc', name: 'Item 8'},
-    { id: 9, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246929/merch/gfctd1ynsdcpchzvgukc', name: 'Item 9'},
-    { id: 10, image: 'https://res.cloudinary.com/dkhpxyxnt/image/upload/v1726246929/merch/gfctd1ynsdcpchzvgukc', name: 'Item 10'},
-];
 
 const ShopGrid: React.FC = () => {
-    const [visibleItems, setVisibleItems] = useState<number>(4);
-    const [isEndOfList, setIsEndOfList] = useState<boolean>(false);
+    // const [visibleItems, setVisibleItems] = useState<number>(4);
+    // const [isEndOfList, setIsEndOfList] = useState<boolean>(false);
 
-    const handleLoadMore = () => {
-        const newVisibleCount = visibleItems + 4;
-        setVisibleItems(newVisibleCount);
+    const { images, loading, error } = useCloudinaryImages('merch');
+
+    // const handleLoadMore = () => {
+    //     const newVisibleCount = visibleItems + 4;
+    //     setVisibleItems(newVisibleCount);
         
-        if (newVisibleCount >= items.length) {
-            setIsEndOfList(true);
-        }
-    };
+    //     if (newVisibleCount >= images.length) {
+    //         setIsEndOfList(true);
+    //     }
+    // };
 
-    const handleLoadLess = () => {
-        setVisibleItems(4);
-        setIsEndOfList(false);
-    };
+    // const handleLoadLess = () => {
+    //     setVisibleItems(4);
+    //     setIsEndOfList(false);
+    // };
+
+    const isMobile = window.innerWidth <= 768;
+    
 
     return (
         <div className="shop-container">
             <h1>SHOP</h1>
+            {loading && <p>Loading images...</p>}
+            {error && <p>Error loading images: {error.message}</p>}
             <div className="shop-grid">
-                {items.slice(0, visibleItems).map(item => (
-                    <div key={item.id} className="grid-item">
-                        <img src={item.image} alt={item.name} />
-                        <div className="grid-item-name">{item.name}</div>
-                    </div>
+                {/* {images.slice(0, visibleItems).map((image) => ( */}
+                {images.slice(0).map((image) => (
+                    <a href={image.URL} key={image.public_id} className="grid-item">
+                        <CloudinaryImage
+                            public_id={image.public_id}
+                            alt={image.alt || image.public_id}
+                            height={isMobile ? 300 : 600}
+                            width={isMobile ? 300 : 600}
+                        />
+                    </a>
                 ))}
             </div>
-            <div className="button-group">
-                {!isEndOfList && visibleItems < items.length && (
+            {/* <div className="button-group">
+                {!isEndOfList && images.length > visibleItems && (
                     <button className="load-more" onClick={handleLoadMore}>
                         Load More
                     </button>
@@ -59,7 +55,7 @@ const ShopGrid: React.FC = () => {
                         Load Less
                     </button>
                 )}
-            </div>
+            </div> */}
         </div>
     );
 };
