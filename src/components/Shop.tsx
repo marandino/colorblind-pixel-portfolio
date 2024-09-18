36fb5a650ae64c378a49e47f4de47e553ad389c5
K3iiX <karaya@marandino.dev>
2024-09-17 19:08:53 -0600

    comment: unnecessary import for now

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import CloudinaryImage from './CloudinaryImage';
 import { useCloudinaryImages } from '../hooks/useCloudinaryImages';
 
