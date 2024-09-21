ea015f04a96279464d0cc795652d5f956bef270f
Chiyen Venegas <chi@marandino.dev>
2024-09-21 16:29:04 -0600

    adds: pagination for brands

--- src/components/Partnerships.tsx
+++ src/components/Partnerships.tsx
@@ -1,8 +1,18 @@
 import { useCloudinaryImages } from "../hooks/useCloudinaryImages";
 import CloudinaryImage from "./CloudinaryImage";
+import { useState, useEffect } from "react";
 
 export default function Partnerships() {
   const { images: brands, loading, error } = useCloudinaryImages("brands");
+  const isMobile = window.innerWidth <= 768;
+  const [brandsToShow, setBrandsToShow] = useState(4);
+
+  useEffect(() => {
+    if (isMobile) {
+      setBrandsToShow(3);
+    }
+    setBrandsToShow(brands.length);
+  }, [isMobile]);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
@@ -11,7 +21,7 @@ export default function Partnerships() {
     <section className="partners" id="partnerships">
       <h1>Partnerships</h1>
       <div className="partners-container">
-        {brands.map((brand) => (
+        {brands.slice(0, brandsToShow).map((brand) => (
           <div className="partner-card" key={brand.public_id}>
             <CloudinaryImage
               public_id={brand.public_id}
@@ -22,6 +32,14 @@ export default function Partnerships() {
             <p className="description">{brand.description}</p>
           </div>
         ))}
+        {brandsToShow < brands.length && (
+          <button
+            className="button"
+            onClick={() => setBrandsToShow(brands.length)}
+          >
+            load more
+          </button>
+        )}
       </div>
     </section>
   );
