e0bf44b31c65c84f24893b380ab18282efa66e4e
Chiyen Venegas <chi@marandino.dev>
2024-10-21 02:51:04 -0600

    fixes: scrolling behaviour

--- src/hooks/useSmoothScroll.ts
+++ src/hooks/useSmoothScroll.ts
@@ -1,8 +1,12 @@
 const useSmoothScroll = () => {
   const scrollToElement = (elementId: string) => {
     const element = document.getElementById(elementId);
+
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      window.scrollTo({
+        top: element.offsetTop - 50,// for some unknown reason, this is the only way to get it working on both iOS and others.
+        behavior: "smooth",
+      });
     }
   };
 
